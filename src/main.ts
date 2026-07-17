7083b9b22fcbd18b84fdcb8d2c2703561e4be925
grahamkatana <graykatanakenny@gmail.com>
2026-07-17 20:51:19 +0200

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, ConsoleLogger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      colors: true,
+      json: true,
+    }),
+  });
   app.useGlobalPipes(new ValidationPipe());
+  
+  const config = new DocumentBuilder()
+    .setTitle('Notes API')
+    .setDescription('API for managing notes')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
